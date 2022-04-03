082b96b7a724b608c647e2f9c12cde8156bae624
Agamura Ian <agamurian@outlook.com>
2022-04-04 00:19:55 +0300

    created hoverable LOGO with text, animations and transitions!

--- src/routes/en/index.svelte
+++ src/routes/en/index.svelte
@@ -3,6 +3,18 @@
     // Libraries
     import {locale, t} from '$lib/translations';
 </script>
+<div class="container my-1">
+  <div class="row">
+<div class="col col-lg sm-col-12 lg-col-4 md-col-6">
+<img src="/xyz.png" alt="" class="img-fluid m-5 p-5 hovery" width="256px" height="256px">  
+</div>
+<div class="col col-lg col-lg-8 col-8">
+  <h2 class="m-5 p-5">HI! I am Agamurian, media artist, developer and 3D Generalist</h2>
+</div>
+
+  </div>
+
+</div>
   <!-- Marketing messaging and featurettes
   ================================================== -->
   <!-- Wrap the rest of the page in another container to center all the content. -->
@@ -50,14 +62,39 @@
       </div><!-- /.col-lg-4 -->
       <div class="col-lg-4">
 
-        <h2>Markdown support</h2>
-        <p>just edit .md files and place them in folders! Inside mdsvex files you can also embed dynamic svelte components or any svelte code.</p>
+        <h2>3D Graphics:</h2>
+<div class="card text-white bg-dark halfblack mb-3" style="max-width: 18rem;">
+  <a href="http://skis.host"><div class="card-header btn h2">SKIS</div></a>
+  <div class="card-body">
+        <h2>3D Graphics:</h2>
+        <p class="card-text">dynamic routing and static site generation, also svelte support</p>
+  </div>
+</div>
+<div class="card text-white bg-dark halfblack mb-3" style="max-width: 18rem;">
+  <a href="http://skis.host"><div class="card-header btn h2">SKIS</div></a>
+  <div class="card-body">
+        <h2>3D Graphics:</h2>
+        <p class="card-text">dynamic routing and static site generation, also svelte support</p>
+  </div>
+</div>
+<div class="card text-white bg-dark halfblack mb-3" style="max-width: 18rem;">
+  <a href="http://skis.host"><div class="card-header btn h2">SKIS</div></a>
+  <div class="card-body">
+        <h2>3D Graphics:</h2>
+        <p class="card-text">dynamic routing and static site generation, also svelte support</p>
+  </div>
+</div>
         <a class="btn btn-secondary" href="https://mdsvex.com/">Документация MDsVEX &raquo;</a>
       </div><!-- /.col-lg-4 -->
       <div class="col-lg-4">
 
-        <h2>Internatiolisation</h2>
-        <p>Just put your files inside folders routes/en and routes/ru - locale switcher is included skis.</p>
+        <h2>System level:</h2>
+<div class="card text-white bg-dark halfblack mb-3" style="max-width: 18rem;">
+  <a href="http://skis.host"><div class="card-header btn h2">SKIS</div></a>
+  <div class="card-body">
+        <h2>3D Graphics:</h2>
+  </div>
+</div>
         <p>Also you can rule global language variables in configs</p>
       </div><!-- /.col-lg-4 -->
     </div><!-- /.row -->
